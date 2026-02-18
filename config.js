b05a82847d17a32b30ec360a5bd122b5264b6642
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-18 18:01:46 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
   bot: {
-    owners: ["" , ""],// owners id
-    botID: "",// bot id
-    GuildId: "", // Your Server Id
-    ClientId: "", // Clietn Id
+    owners: ["1035345058561540127" , ""],// owners id
+    botID: "1279423025980112988",// bot id
+    GuildId: "1275275223431647355", // Your Server Id
+    ClientId: "1279423025980112988", // Clietn Id
     serverinvte: "", // Server invite url
-    clientSECRET: "", // Client Secret
+    clientSECRET: "hcOZiiMkVIx5kTDfNtEx-1QLm5GZuHl4", // Client Secret
     callbackURL: "", // Call Back Url
-    inviteBotUrl: "",// Link Invite Bot
+    inviteBotUrl: "https://discord.com/oauth2/authorize?client_id=1279423025980112988&permissions=8&integration_type=0&scope=bot",// Link Invite Bot
     verifylink: "",// Verification Bot
     category: "",// Ticket Category
     probotid: "",// ProBot Id

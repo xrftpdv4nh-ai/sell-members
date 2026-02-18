85917df6eab394d3dd43d872135b0119722e7714
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-18 18:50:40 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ module.exports = {
     botID: "1279423025980112988",// bot id
     GuildId: "1275275223431647355", // Your Server Id
     ClientId: "1279423025980112988", // Clietn Id
-    serverinvte: "", // Server invite url
+    serverinvte: "https://discord.gg/aq3Gdsc5K", // Server invite url
     clientSECRET: "hcOZiiMkVIx5kTDfNtEx-1QLm5GZuHl4", // Client Secret
     callbackURL: "https://sell-members-production.up.railway.app/login", // Call Back Url
     inviteBotUrl: "https://discord.com/oauth2/authorize?client_id=1279423025980112988&permissions=8&integration_type=0&scope=bot",// Link Invite Bot
     verifylink: "https://sell-members-production.up.railway.app/login",
-    category: "",// Ticket Category
-    probotid: "",// ProBot Id
-    transferid: "",// Id of the person to whom the balance is being transferred
-    price: "",// Member Price
-    logs: ""// Logs Room Id
+    category: "1470482518120861920",// Ticket Category
+    probotid: "282859044593598464",// ProBot Id
+    transferid: "1066773224723058778",// Id of the person to whom the balance is being transferred
+    price: "1",// Member Price
+    logs: "1275933830905335819"// Logs Room Id
   },
   website: {
     PORT: "",//Website Port

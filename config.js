6d85c07c4f90667d0e4a620613997ab9c29e99d2
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-18 18:26:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
     ClientId: "1279423025980112988", // Clietn Id
     serverinvte: "", // Server invite url
     clientSECRET: "hcOZiiMkVIx5kTDfNtEx-1QLm5GZuHl4", // Client Secret
-    callbackURL: "", // Call Back Url
+    callbackURL: "sell-members-production.up.railway.app", // Call Back Url
     inviteBotUrl: "https://discord.com/oauth2/authorize?client_id=1279423025980112988&permissions=8&integration_type=0&scope=bot",// Link Invite Bot
     verifylink: "",// Verification Bot
     category: "",// Ticket Category

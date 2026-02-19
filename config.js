b43a478c1ab0b654f4dc39be9af670fc5037b4b1
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-19 20:22:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,4 @@
 module.exports = {
-  bot: {
-    token: process.env.token,
-    clientId: "1279423025980112988",
-    owners: ["1035345058561540127"],
-
-    verifyLink: "https://discord.com/oauth2/authorize?client_id=CLIENT_ID&response_type=code&scope=identify",
-
-    categoryId: "1470482518120861920",
-    price: 10,
-    logs: "1275933830905335819"
-  }
+  prefix: "+",
+  ticketCategory: "PUT_CATEGORY_ID_HERE"
 };

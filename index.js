926a33e72a59844df3d1014ad10fb0d639b3a0bd
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-20 00:54:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ if (!BOT_TOKEN) {
 const client = new Client({
   intents: [
     Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.MESSAGE_CONTENT
   ],
 });
 

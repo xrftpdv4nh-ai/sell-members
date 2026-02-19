c28418414930bb85264fa150077abacb8dbb9df2
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-19 23:27:06 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,15 @@
 module.exports = {
+  // ===== Prefix =====
   prefix: "+",
+
+  // ===== Admin / Owners =====
+  owners: [
+    "1035345058561540127" // Admin ID
+  ],
+
+  // ===== Ticket System =====
   ticket: {
-    categoryId: "1474149798734073946",
-    supportRoleId: "1474149490360586340"
+    categoryId: "1474149798734073946", // Category ID بتاع التكتات
+    supportRoleId: "1474149490360586340" // Role الدعم
   }
 };

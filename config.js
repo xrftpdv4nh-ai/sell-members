733f4c6bf7d79b9f447a4275683f95c6c79a0ef8
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-20 05:45:56 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,27 +1,27 @@
 module.exports = {
   "bot": {
-    "owners": [""],
-    "botID": "",
-    "GuildId": "",
-    "ClientId": "",
-    "serverinvte": "https://discord.gg/code",
-    "clientSECRET": "",
-    "callbackURL": "http://Adress:Port/login",
-    "inviteBotUrl": "",
-    "TheLinkVerfy": "",
+    "owners": ["1035345058561540127"],
+    "botID": "1279423025980112988",
+    "GuildId": "1275275223431647355",
+    "ClientId": "1474149543686832299",
+    "serverinvte": "https://discord.gg/aCHmYayjKR",
+    "clientSECRET": "m9CmE9FRoOFdOGyJRniktK-9E3vfirKq",
+    "callbackURL": "https://sell-members-production.up.railway.app/login",
+    "inviteBotUrl": "https://discord.com/oauth2/authorize?client_id=1279423025980112988&permissions=8&integration_type=0&scope=bot",
+    "TheLinkVerfy": "https://sell-members-production.up.railway.app/login",
     "prefix": "-",
-    "ceatogry": "1374426929117008044",
-    "TOKEN": "",
-    "TraId": "",
+    "ceatogry": "1474149798734073946",
+    "TOKEN": "MTI3OTQyMzAyNTk4MDExMjk4OA.G_k_SB.KPboUdwqTgvJtTHL8llo_B3lh331pF3vNnH2Ns",
+    "TraId": "1035345058561540127",
     "price": "1",
-    "channelId": "",
-    "roleId": "",
-    "LineIce": "",
-    "taxchannels": "",
+    "channelId": "1474150022713970913",
+    "roleId": "1474149543686832299",
+    "LineIce": "https://cdn.discordapp.com/attachments/1474149853838704953/1474250405729931419/Line.png?ex=69992a2e&is=6997d8ae&hm=93d1c666b493e16d02a0767cdb1582eec684b2b88f7937cb1a37a78f2f9312d9&",
+    "taxchannels": "1474150022713970913",
     "coinprice": "1"
   },
   "Log": {
-    "LogChannelOwners": ""
+    "LogChannelOwners": "1474150012228210868"
   },
   "website": {
     "PORT": ""

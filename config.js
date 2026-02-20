ca4e544eb2ba17100e619f423b9c506c962d7247
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-20 04:13:31 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ module.exports = {
 
   adminId: "1035345058561540127",
 
-  // ===== ProBot =====
-  probotId: "282859044593598464", // ID بروبوت الرسمي
-  creditAccountId: "1308224732625375232", // الحساب اللي يستقبل الكريدت
+  probot: {
+    id: "282859044593598464",          // ID بروبوت
+    creditAccountId: "1308224732625375232"
+  },
 
-  // ===== Tickets =====
   ticket: {
     categoryId: "1474149798734073946",
     supportRoleId: "1474149490360586340"

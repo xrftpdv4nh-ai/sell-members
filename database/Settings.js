b3c87e0908a417d780ddf82cf6e5aa9d5872d57f
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-22 01:22:43 +0200

    Update Settings.js

--- database/Settings.js
+++ database/Settings.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
-const settingsSchema = new mongoose.Schema({
+const schema = new mongoose.Schema({
   guildId: { type: String, unique: true },
-  verifiedRole: { type: String, default: null }
+  verifiedRole: String
 });
 
-module.exports = mongoose.model("GuildSettings", settingsSchema);
+module.exports = mongoose.model("GuildSettings", schema);

b4aa848c89ff44c66357237362513dd3f7068eea
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-21 14:34:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,28 +1,28 @@
 module.exports = {
-  "bot": {
-    "owners": ["1035345058561540127"],
-    "botID": "1279423025980112988",
-    "GuildId": "1275275223431647355",
-    "ClientId": "1279423025980112988",
-    "serverinvte": "https://discord.gg/aCHmYayjKR",
-    "clientSECRET": "m9CmE9FRoOFdOGyJRniktK-9E3vfirKq",
-    "callbackURL": "https://sell-members-production.up.railway.app/login",
-    "inviteBotUrl": "https://discord.com/oauth2/authorize?client_id=CLIENT_ID_HERE&permissions=8&scope=bot%20applications.commands",
-    "TheLinkVerfy": "https://discord.com/oauth2/authorize?client_id=1279423025980112988&response_type=code&redirect_uri=https%3A%2F%2Fsell-members-production.up.railway.app%2Flogin&scope=identify+guilds+guilds.join",
-    "prefix": "-",
-    "ceatogry": "1474149798734073946",
-    "TraId": "1035345058561540127",
-    "price": "1",
-    "channelId": "1474150022713970913",
-    "roleId": "1474149543686832299",
-    "LineIce": "https://cdn.discordapp.com/attachments/1474149853838704953/1474250405729931419/Line.png?ex=69992a2e&is=6997d8ae&hm=93d1c666b493e16d02a0767cdb1582eec684b2b88f7937cb1a37a78f2f9312d9&",
-    "taxchannels": "1474150022713970913",
-    "coinprice": "1"
+  bot: {
+    token: process.env.BOT_TOKEN,
+    clientId: "1279423025980112988",
+    clientSecret: "m9CmE9FRoOFdOGyJRniktK-9E3vfirKq",
+    guildId: "1275275223431647355",
+    prefix: "+",
+    owners: ["1035345058561540127"],
+
+    inviteBotUrl:
+      "https://discord.com/oauth2/authorize?client_id=CLIENT_ID_HERE&permissions=8&scope=bot%20applications.commands"
   },
-  "Log": {
-    "LogChannelOwners": "1474150012228210868"
+
+  oauth: {
+    loginURL: "/login",
+    callbackURL: "/callback",
+    scopes: ["identify", "guilds", "guilds.join"]
   },
-  "website": {
-    "PORT": "3000"
+
+  logs: {
+    success: "1474150012228210868",
+    failed: "1474150017810829312"
+  },
+
+  members: {
+    delay: 5000 // 5 ثواني بين كل عضو
   }
 };

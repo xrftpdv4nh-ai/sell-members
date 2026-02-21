4dc61bdbb7696ded71cbd8a9f06636da41f9e49b
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-21 22:28:19 +0200

    Update checkToken.js

--- utils/checkToken.js
+++ utils/checkToken.js
@@ -1,14 +1,16 @@
+// utils/checkToken.js
 const axios = require("axios");
 
 module.exports = async function checkToken(accessToken) {
   try {
-    await axios.get("https://discord.com/api/users/@me", {
+    const res = await axios.get("https://discord.com/api/users/@me", {
       headers: {
         Authorization: `Bearer ${accessToken}`
       }
     });
-    return true;
-  } catch {
+
+    return res.status === 200;
+  } catch (err) {
     return false;
   }
 };

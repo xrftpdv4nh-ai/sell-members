f7c7fa028374a854e7154f911c0c2505b91b73ea
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-21 23:13:49 +0200

    Update setrole.js

--- commands/setrole.js
+++ commands/setrole.js
@@ -15,7 +15,6 @@ module.exports = {
       return message.reply("❌ منشن الرول أو حط الـ ID");
     }
 
-    // تأكد إن البوت أعلى من الرول
     if (role.position >= message.guild.me.roles.highest.position) {
       return message.reply("❌ الرول أعلى من البوت");
     }

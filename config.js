2fa25e08173f707f1900306136d7c529c28aef20
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-22 02:16:54 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,27 +1,35 @@
 module.exports = {
   bot: {
     // ===== BOT =====
-    token: process.env.BOT_TOKEN, // ⬅️ خلي التوكن من Railway
+    token: process.env.BOT_TOKEN, // ⬅️ توكن البوت من Railway
     clientId: "1279423025980112988",
-    clientSecret: "m9CmE9FRoOFdOGyJRniktK-9E3vfirKq", // ⬅️ سيكريت OAuth
-    guildId: "1275275223431647355",
+    clientSecret: "m9CmE9FRoOFdOGyJRniktK-9E3vfirKq", // ⚠️ يفضل تخليه ENV بعدين
+
     prefix: "+",
     owners: ["1035345058561540127"],
+
+    // ===== السيرفر الأساسي =====
+    guildId: "1275275223431647355", // ✅ ID السيرفر اللي الرول هيتضاف فيه
+
+    // ===== رول التوثيق =====
+    verifiedRoleId: null, 
+    // ⬅️ هيتحدد بالأمر: +setrole @Verified
 
     // ===== زر إدخال البوت =====
     inviteBotUrl:
       "https://discord.com/oauth2/authorize?client_id=1279423025980112988&permissions=8&scope=bot%20applications.commands",
 
     // ===== زر اثبت نفسك (OAuth Verify) =====
-    TheLinkVerfy:"https://discord.com/oauth2/authorize?client_id=1279423025980112988&response_type=code&redirect_uri=https%3A%2F%2Fsell-members-production.up.railway.app%2Fcallback&scope=identify+guilds+guilds.join"
+    TheLinkVerfy:
+      "https://discord.com/oauth2/authorize?client_id=1279423025980112988&response_type=code&redirect_uri=https%3A%2F%2Fsell-members-production.up.railway.app%2Fcallback&scope=identify+guilds+guilds.join"
   },
 
   // ===== OAUTH =====
   oauth: {
-  loginURL: "/login",
-  callbackURL: "https://sell-members-production.up.railway.app/callback",
-  scopes: ["identify", "guilds", "guilds.join"]
-},
+    loginURL: "/login",
+    callbackURL: "https://sell-members-production.up.railway.app/callback",
+    scopes: ["identify", "guilds", "guilds.join"]
+  },
 
   // ===== LOG CHANNELS =====
   logs: {
